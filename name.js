e9c0847cb204c930279399719d048bc7354c2426
Keshav Carpenter <20uec068@lnmiit.ac.in>
2023-06-05 00:24:49 +0530

    "committed to github forever :) "

--- name.js
+++ name.js
@@ -1,2 +1,20 @@
-const os = require('os')
-console.log(os)
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req)
+    if (req.url === '/') {
+        res.end('Welcome to our home page')
+        return
+    }
+    if (req.url === '/about') {
+        res.end('Here is our short history')
+        return
+    }
+    res.end(`
+    <h1>Oops!</h1>
+    <p>We can't seem to find the page you are looking for</p>
+    <a href="/">back home</a>
+    `)
+})
+
+server.listen(5000)
